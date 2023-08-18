69ad7361219a5cc2d7f3ee3261a427d88dde46ed
mkm0702 <mkm16207@gmail.com>
2023-08-18 18:22:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+// Define a route to handle the root URL
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Define the port to listen on
+const port = 3000;
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}/`);
+});
